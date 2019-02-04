7bb4ff400117e784ab38de1ec2705b8239f6590c
Jonathan <JonathanH@mixtelematics.com>
2019-02-04 09:34:00 +0200

    done with basic user in put

--- JustJava/app/src/main/java/com/example/justjava/MainActivity.java
+++ JustJava/app/src/main/java/com/example/justjava/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.justjava;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -11,7 +12,7 @@ import java.text.NumberFormat;
 
 public class MainActivity extends AppCompatActivity {
 
-    private int quantity = 0, price;
+    private int quantity = 2, price;
     private  String total;
     private CheckBox whippedCream, chocolate;
     private EditText name;
@@ -20,7 +21,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        createOrderSummary();
+        //createOrderSummary();
 
         whippedCream = (CheckBox) findViewById(R.id.whipped_cream_checkbox);
         chocolate = (CheckBox) findViewById(R.id.chocolate_checkbox);
@@ -31,20 +32,33 @@ public class MainActivity extends AppCompatActivity {
 
     public void addQuantity(View view)
     {
-        quantity += 1;
-        displayQuantity(quantity);
-
+        if (quantity < 100) {
+            quantity += 1;
+            displayQuantity(quantity);
+        }
     }
 
     public void removeQuantity(View view)
     {
-        quantity -= 1;
-        displayQuantity(quantity);
-
+        if (quantity > 1) {
+            quantity -= 1;
+            displayQuantity(quantity);
+        }
     }
 
     private void calculatePrice(int quantity) {
-        price = quantity * 5;
+
+        price = 0;
+        if (whippedCream.isChecked())
+        {
+            price += 1;
+        }
+        if (chocolate.isChecked())
+        {
+            price += 2;
+        }
+        price += 5;
+        price = price * quantity;
     }
 
     public void submitOrder(View view) {
@@ -62,13 +76,24 @@ public class MainActivity extends AppCompatActivity {
         String priceMessage = "Free";
         if (quantity > 0)
         {
-            priceMessage = "Name: " + name.getText() + "\nAdd whipped Cream? " + whippedCream.isChecked() + "\nAdd chocolate? " + chocolate.isChecked() +" \nQuantity: " + quantity + " \nTotal: " + NumberFormat.getCurrencyInstance().format(price) + "\nThank you!";
+            priceMessage = getString(R.string.order_summary_email_subject, name.getText());
+            priceMessage += "\n" + getString(R.string.order_summary_whipped_cream, (Boolean)whippedCream.isChecked());
+            priceMessage += "\n" + getString(R.string.order_summary_chocolate, (Boolean)chocolate.isChecked());
+            priceMessage += "\n" + getString(R.string.order_summary_quantity, quantity);
+            priceMessage += "\n" + getString(R.string.order_summary_price, NumberFormat.getCurrencyInstance().format(price));
+            priceMessage += "\n" + getString(R.string.thank_you);
         }
 
 
         TextView orderSummaryTextView = (TextView) findViewById(R.id.order_summery_text_view);
         orderSummaryTextView.setText(priceMessage);
 
+        Intent emailIntent = new Intent(Intent.ACTION_SEND);
+        emailIntent.setType("*/*");
+        emailIntent.putExtra(Intent.EXTRA_EMAIL, "JustJava@gmail.com");
+        emailIntent.putExtra(Intent.EXTRA_SUBJECT, getString(R.string.order_summary_email_subject, name.getText()));
+        emailIntent.putExtra(Intent.EXTRA_TEXT, priceMessage);
+       // startActivity(emailIntent);
     }
 
     /**
